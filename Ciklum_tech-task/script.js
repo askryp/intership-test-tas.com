eb3b95d776256c10d5b5c5ef39d137f85b80f5c4
askryp <43927171+askryp@users.noreply.github.com>
2021-02-08 21:09:39 +0200

    Add files via loading

---
+++ Ciklum_tech-task/script.js
@@ -0,0 +1,12 @@
+const cancel = document.getElementById("cancel");
+// var create = document.getElementById("create");
+const newEvent = document.getElementById("new-event");
+
+function createNewEvent () {
+    newEvent.onclick;
+    location.href = "http://localhost:63342/Ciklum_tech-task/create_event.html";
+}
+function cancelNewEvent (){
+    cancel.onclick;
+    location.href = "http://localhost:63342/Ciklum_tech-task/index.html";
+}
